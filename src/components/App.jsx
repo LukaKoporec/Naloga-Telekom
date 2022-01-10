9573050001e4793044dc2a32dfa426775d07a5fd
Luka Koporec <58914727+LukaKoporec@users.noreply.github.com>
2022-01-10 21:43:48 +0100

    7.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import About from "./About";
+import Footer from "./Footer";
 
-export default function App() {
+function App() {
   return (
     <div>
       <Header />
       <About />
+      <Footer />
     </div>
   );
 }
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Styles/Footer.scss";
+
+function Footer() {
+  return (
+    <footer id="footer-new">
+      <div>
+        <h3>Pridružite se nam na družbenih omrežjih</h3>
+        <p>Sveže novice, akcije in ponudbe.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
