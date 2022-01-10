fb5737584fd6a740673a74e7eaa7e3325c02663b
Luka Koporec <58914727+LukaKoporec@users.noreply.github.com>
2022-01-10 19:03:59 +0100

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "../Styles/About.scss";
+
+function About() {
+  return (
+    <div>
+      <p id="start">
+        Paket NEO Svet B poleg SVETOVNE mobilne telefonije, televizije,
+        interneta ter Fiksne telefonije novim in obstoječim naročnikom ponuja
+        tudi naprave po SVETOVNIH cenah.
+      </p>
+      <h2>Paket NEO Svet B</h2>
+      <ol>
+        <ul>
+          <hr />
+          <h3>MOBILNA TELEFONIJA</h3>
+          <hr />
+          <li>
+            <b>neomejeni</b> klici in sporočila SMS/MMS v vsa slovenska mobilna
+            in fiksna omrežja in v državah območja EU-tarife
+          </li>
+          <li>
+            <b>200 GB</b> prenos podatkov v Sloveniji, od tega do{" "}
+            <b>16.106 MB</b> brezplačno v državah območja EU-tarife
+          </li>
+          <li>
+            hitrost mobilnega interneta do <b>150/50 Mbit/s</b>
+          </li>
+          <li>
+            možnost do <b>4</b> dodatne številke Druga številka - NEO
+          </li>
+        </ul>
+        <ul>
+          <hr />
+          <h3>TELEVIZIJA</h3>
+          <hr />
+          <li>programska shema Mega HD</li>
+          <li>vključen prvi NEO Smartbox</li>
+          <li>
+            <b>ogled nazaj</b> za 7 dni in <b>snemalnik</b>
+          </li>
+          <li>predpripravljen vmesnik za storitev Pametni dom</li>
+        </ul>
+        <ul>
+          <hr />
+          <h3>INTERNET</h3>
+          <hr />
+          <li>
+            hitrost do <b>350/100 Mbit/s</b> na optiki (do 25/5 Mbit/s na VDSL,
+            do 20/1 Mbit/s na ADSL)
+          </li>
+          <li>modem z možnostjo WiFi brezplačno</li>
+          <li>možnost razširitve omrežja WiFi s storitvijo Turbo Wifi</li>
+        </ul>
+        <ul>
+          <hr />
+          <h3>FIKSNA TELEFONIJA</h3>
+          <hr />
+          <li>3.000 minut klicev v vsa slovenska fiksna in mobilna omrežja</li>
+        </ul>
+      </ol>
+      <p id="cena"> Redna cena paketa: 74 €</p>
+      <h2>Naj NEO naprave</h2>
+      <div class="aligncenter">
+        <div>
+          <a href="https://www.telekom.si/zasebni-uporabniki/ponudba/e-trgovina/izdelek/telefonija/mobiteli/apple-iphone-13-128gb-crna">
+            <img
+              src="https://ts.sdn.si/hybris/1000/70031663_00/apple-iphone-13-128gb-crna/001.jpg"
+              alt="Apple iPhone 13"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.telekom.si/zasebni-uporabniki/ponudba/e-trgovina/izdelek/sport-in-prosti-cas/pametne-ure-in-zapestnice/apple-pametna-ura-watch-series-s7-gps-41mm-sport-band-mkmx3bs-a-41-mm-crna-s-crnim-pasckom-sb">
+            <img
+              src="https://ts.sdn.si/hybris/1000/70031976_00/apple-pametna-ura-watch-series-s7-gps-41mm-sport-band-mkmx3bs-a-41-mm-crna-s-crnim-pasckom-sb/001.jpg"
+              alt="Apple Pametna ura Series 7"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.telekom.si/zasebni-uporabniki/ponudba/e-trgovina/izdelek/racunalnistvo/igralne-konzole/sony-playstation5-825gb-bela">
+            <img
+              src="https://ts.sdn.si/hybris/1000/70028128_00/sony-playstation5-825gb-bela/001.jpg"
+              alt="Sony PlayStation 5"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Styles/Header.scss";
+
+function Header() {
+  return (
+    <header id="header-new">
+      <div>
+        <span>POLOVIČNA NAROČNINA ZA NOVE IN OBSTOJEČE NAROČNIKE</span>
+        <h1>
+          PAKET <b>NEO SVET B</b>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
